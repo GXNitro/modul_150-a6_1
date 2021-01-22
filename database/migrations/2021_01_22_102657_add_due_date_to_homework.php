3a912caf63f4508f219f500913bd58990993f1e6
GXNitro <62222712+GXNitro@users.noreply.github.com>
2021-01-22 15:51:06 +0100

    Update

--- database/migrations/2021_01_22_102657_add_due_date_to_homework.php
+++ database/migrations/2021_01_22_102657_add_due_date_to_homework.php
@@ -26,7 +26,7 @@ class AddDueDateToHomework extends Migration
     public function down()
     {
         Schema::table('homework', function (Blueprint $table) {
-            //
+            $table->dropColumn('date');
         });
     }
 }
